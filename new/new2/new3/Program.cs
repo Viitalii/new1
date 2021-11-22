c9df0b5033c40fa980b3319a8b5d9e14a68d01fa
vitalii <dr.vitalik@mail.ru>
2021-11-22 11:33:05 +0300

    wow

---
+++ new/new2/new3/Program.cs
@@ -0,0 +1,16 @@
+﻿string[] array = { "пн", "вт", "ср", "чт", "пт", "сб", "вск"};
+
+Console.Write("Введите номер дня недели:");
+
+int Dayofweek = int.Parse(Console.ReadLine());
+
+Console.WriteLine(array [Dayofweek-1]);
+
+if (Dayofweek > 7 || Dayofweek < 1)
+{
+    Console.Write("Неверный номер дня в недели");
+}
+else
+{
+    Console.WriteLine(array [Dayofweek-1]);
+}
